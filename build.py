15d7cc4d7d9734c441750f5d3fb983ff90c32da9
Mustard <info@mstrd.eu>
2024-12-11 12:14:18 +0100

    optimize build script

--- build.py
+++ build.py
@@ -4,8 +4,12 @@ files = [] # List of files to be compiled.
 
 for file in os.listdir('./src/'):
     if file.endswith('.kt'):
-        files.append(file)
+        files.append('./src/' + file)
 
-if len(files) > 0:
+amount = len(files)
+if amount > 0:
+    echo = "Finished compiling " + str(amount) + " file"
+    if amount > 1:
+        echo += "s"
     cmd = 'kotlinc ' + ' '.join(files) + ' -include-runtime -d app.jar'
-    os.system(cmd)
+    os.system(cmd), print(echo)
