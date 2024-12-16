6d6a0304c59761b07d6c06a5b8342f1ccd62093e
Mustard <info@mstrd.eu>
2024-12-16 22:07:59 +0100

    print generated keys in Main.kt

--- client/src/Main.kt
+++ client/src/Main.kt
@@ -1,3 +1,11 @@
-fun main() {
+import services.KeygenService
+import java.util.Base64
 
+fun main() {
+    val keygenService = KeygenService()
+    val keyPair = keygenService.generateKeyPair()
+    val publicKeyString = Base64.getEncoder().encodeToString(keyPair.public.encoded)
+    val privateKeyString = Base64.getEncoder().encodeToString(keyPair.private.encoded)
+    println("Public key: $publicKeyString")
+    println("Private key: $privateKeyString")
 }
